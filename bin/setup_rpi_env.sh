83c2032cd69f6e0337e5a942dc0cb0b8f1cb5b60
dsnyder0pc <dsnyder0cnn@gmail.com>
2025-12-07 14:58:17 -0700

    girl

--- bin/setup_rpi_env.sh
+++ bin/setup_rpi_env.sh
@@ -39,7 +39,7 @@ APT_DEPS=(
   git build-essential autoconf automake libtool zlib1g-dev libbz2-dev liblzma-dev libexpat1-dev libffi-dev \
   libssl-dev libncurses5-dev libncursesw5-dev libreadline-dev uuid-dev libdb-dev libgdbm-dev libsqlite3-dev \
   vim shellcheck tmux mosh tk tk-dev \
-  fonts-noto-color-emoji slurp wl-clipboard \
+  fonts-noto-color-emoji gir1.2-peas-1.0 slurp wl-clipboard \
   jq yq
 )
 
